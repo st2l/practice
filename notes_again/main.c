9729f26cce2598c055d182057e57936650abd5e9
Дольский Степан Андреевич <sadolskii@mts.ru>
2026-02-26 21:54:35 +0300

    Added new task

---
+++ notes_again/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char storage[10][64];
+int g_count = 0;
+
+int main(int argc, char *argv[]) {
+
+  puts("Welcome to our new notes manager!");
+  puts("TOTALLY not vulnerable notes manager!");
+  puts("If u will find any vulnerability - u will get a flag!!!");
+
+  int idx;
+  int option;
+  char name[64];
+  char *arr[10];
+
+  while (1) {
+    puts("Choose option:\n1) Change name\n2) Create note\n3) Edit note\n0) Exit");
+    scanf("%d", &option);
+    printf("%d", option);
+
+    switch (option) {
+      case 0:
+        goto exit_loop;
+        break;
+
+      case 1:
+        puts("Enter your name: ");
+        scanf("%s", name);
+        printf("Well... your new name! -> %s", name);
+        break;
+
+      case 2:
+        if (g_count == 9) {
+          puts("No more notes for you today...");
+          goto exit_loop;
+        }
+        puts("Enter your new note!");
+        arr[g_count] = storage[g_count];
+        scanf("%63s", arr[g_count]);
+        g_count += 1;
+        puts("Seccessfully saved!");
+        break;
+
+      case 3:
+        puts("Enter index of note");
+        scanf("%d", &idx);
+        puts("Now write new data to your note!");
+        scanf("%s", arr[idx]);
+        puts("Nice!");
+        break;
+    }
+  }
+exit_loop:
+  puts("Bye!");
+}
